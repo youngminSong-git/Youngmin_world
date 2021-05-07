e34b8d60736cf75cd5699af64b1c3b81a87ec9ae
Song-young-min <83380965+Song-young-min@users.noreply.github.com>
2021-05-07 12:10:49 +0900

    0507최초커밋

---
+++ 20210507/src/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+	String accNum,
+		   accName;
+	int balance; //입금과 출금으로 나뉘어짐
+	
+	public Account(String accNum, String accName, int balance) {
+		this.accNum = accNum;
+		this.accName = accName;
+		this.balance = balance;
+	}
+	
+	public void setDeposit(int money) {
+		this.balance += money;
+	}
+}
